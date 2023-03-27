49a825dec7cdc7f554cbcf74ddbe04bb51cee4d1
Farzan-TekHQS <farzan.hassan@tekhqs.com>
2023-03-27 15:00:59 +0500

    Banner added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import BannerModule from "./Banner.module.css";
+
+function Banner() {
+  return <div className={BannerModule.banner}></div>;
+}
+
+export default Banner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,6 +15,7 @@ import AdbIcon from "@mui/icons-material/Adb";
 import { color } from "@mui/system";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import { alignProperty } from "@mui/material/styles/cssUtils";
+import NavbarModule from "./Navbar.module.css";
 
 const pages = ["Home", "Jobs", "Companies", "Candidates", "Pages", "Blogs"];
 const subPages = ["Sub 1", "Sub 2", "Sub 3"];
@@ -49,75 +50,80 @@ const Navbar = () => {
   };
 
   return (
-    <AppBar
-      position="static"
-      elevation={0}
-      style={{ background: "transparent", borderBlock: "none" }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon
-            sx={{
-              display: { xs: "none", md: "flex" },
-              mr: 1,
-              color: "#555555",
-            }}
-          />
-          <Typography variant="h6" href="/" sx={{ ...sx, color: "#555555" }}>
-            LOGO
-          </Typography>
+    <div className={NavbarModule.navbar}>
+      <AppBar
+        position="static"
+        elevation={0}
+        style={{
+          background: "transparent",
+          boxShadow: "none",
+        }}
+      >
+        <Container maxWidth="xl" mr={3}>
+          <Toolbar disableGutters>
+            <AdbIcon
+              sx={{
+                display: { xs: "none", md: "flex" },
+                mr: 1,
+                color: "#555555",
+              }}
+            />
+            <Typography variant="h6" href="/" sx={{ ...sx, color: "#555555" }}>
+              LOGO
+            </Typography>
 
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "none", md: "flex" },
-              // ":hover": { backgroundColor: "transparent" },
-            }}
-          >
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{
-                  my: 2,
-                  color: "#555555",
-                  "&:hover": { backgroundColor: "white" },
-                }}
-              >
-                {page}
-                <ArrowDropDownIcon />
-              </Button>
-            ))}
-          </Box>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex" },
+                // ":hover": { backgroundColor: "transparent" },
+              }}
+            >
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{
+                    my: 2,
+                    color: "#555555",
+                    "&:hover": { backgroundColor: "none" },
+                  }}
+                >
+                  {page}
+                  <ArrowDropDownIcon />
+                </Button>
+              ))}
+            </Box>
 
-          <Button
-            sx={{
-              my: 2,
-              mr: 2,
-              color: "#555555",
-              "&:hover": { backgroundColor: "white" },
-            }}
-          >
-            Login
-          </Button>
-          <Button
-            sx={{
-              my: 2,
-              color: "white",
-              "&:hover": { backgroundColor: "white" },
-              backgroundColor: "#35554d",
-              borderRadius: 16,
-              pl: 3,
-              pr: 3,
-              pt: 1,
-              pb: 1,
-            }}
-          >
-            Post a Job
-          </Button>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <Button
+              sx={{
+                my: 2,
+                mr: 2,
+                color: "#555555",
+                "&:hover": { backgroundColor: "none" },
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              sx={{
+                my: 2,
+                color: "white",
+                "&:hover": { backgroundColor: "none" },
+                backgroundColor: "#35554d",
+                borderRadius: 16,
+                pl: 3,
+                pr: 3,
+                pt: 1,
+                pb: 1,
+              }}
+            >
+              Post a Job
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
   );
 };
 
