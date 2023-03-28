3cabfe6fed49fee4d8ce5fe60043de9cc4786a41
Farzan-TekHQS <farzan.hassan@tekhqs.com>
2023-03-28 07:38:32 +0500

    Added p's next to search bar

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,17 @@ import BannerModule from "./Banner.module.css";
 import SearchBar from "./SearchBar";
 
 function Banner() {
-  return <div className={BannerModule.banner}>
-    <SearchBar />
-  </div>;
+  return (
+    <div className={BannerModule.banner}>
+      <div className={BannerModule.p1}>
+        <p>12K Jobs in 200 countries</p>
+      </div>
+      <SearchBar />
+      <div className={BannerModule.p}>
+        <p>Popular Searches: Finance Manager Business Manager</p>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
